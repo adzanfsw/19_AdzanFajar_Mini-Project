bb71dfa8b0d233e26d0d5eb2e15090ff151ed71f
adzanfsw <90901471+adzanfsw@users.noreply.github.com>
2021-09-28 18:49:59 +0700

    Page Artikel

--- src/page/TentangKami.jsx
+++ src/page/TentangKami.jsx
@@ -15,12 +15,14 @@ function TentangKami() {
             <h1>TENTANG JUSTRUN<small>!</small></h1>
 
             <p>Kami adalah sebuah platform informasi terpercaya dalam hal sepatu lari. Platform ini dibuat berdasar alasan karena olahraga yang paling murah dan mudah dilakukan adalah berlari. Oleh karena itu, kami hadir untuk membantu masyarakat maupun komunitas dalam menentukan senjata terbaik untuk menapaki medan dalam berolahraga lari ataupun yang lain.</p>
-            <p>Di website ini, pengguna bisa saling satu sama lain untuk memberikan pendapat dan pengalam terhadap produk tertentu, yang nantinya diharapkan bisa untuk membantu teman-teman yang lain dalam menilai sebelum memutuskan untuk membeli sebuah produk. Terima kasih, dan semangat!!</p>
+            <p>Di website ini, pengguna bisa saling satu sama lain untuk memberikan pendapat dan pengalam terhadap produk tertentu, yang nantinya diharapkan bisa untuk membantu teman-teman yang lain dalam menilai sebelum memutuskan untuk membeli sebuah produk. Terima kasih, dan semangat!!</p> <br />
+
+            <strong>Kontak kami <small>&gt;</small></strong>
         </article>
 
-        <footer>
-            © 2021 JUSTRUN! All Rights Reserved.
-        </footer>
+        <footer>	
+   			<p>© 2021 JUSTRUN! All Rights Reserved.</p>
+   		</footer>
     </>
   );
 }
